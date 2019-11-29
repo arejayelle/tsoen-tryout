2fd207862d1b1a9bd039f299596fd73e02fb7f45
Brianne O'Gallagher-Roy <brianne13roy@live.com>
2019-11-29 15:39:02 -0500

    add notifySurvivor method, add location attribute to all the types (soldier, citizen, merchant)

--- src/immortuos/solution/Application.java
+++ src/immortuos/solution/Application.java
@@ -35,7 +35,6 @@ public class Application {
      * @param type The type of this survivor.
      */
     public void registerSurvivor(Survivor survivor) {
-        // Write your code here.
         this.survivorList.add(survivor);
         survivor.notify(new Event("registered", survivor.getLocation()));
     }
@@ -64,58 +63,56 @@ public class Application {
 
     public void waterEvent(Event event){
         for(Survivor survivor: this.survivorList){
-            int distance = this.distance(event.getLocation(), survivor.getLocation());
-            if(distance <= WATER_DISTANCE){
-                survivor.notify(event);
-            }
+            this.notifySurvivor(event, survivor, WATER_DISTANCE);
         }
     }
 
     public void tradeEvent(Event event){
         for(Survivor survivor: this.survivorList) {
-            int distance = this.distance(event.getLocation(), survivor.getLocation());
             switch (survivor.getType()) {
                 case "citizen":
-                    if (distance <= TRADE_CITIZEN_DISTANCE) {
-                        survivor.notify(event);
-                    }
+                    this.notifySurvivor(event, survivor, TRADE_CITIZEN_DISTANCE);
                     break;
                 case "merchant":
-                    if (distance <= TRADE_MERCHANT_DISTANCE) {
-                        survivor.notify(event);
-                    }
+                    this.notifySurvivor(event, survivor, TRADE_MERCHANT_DISTANCE);
             }
         }
     }
 
     public void zombieEvent(Event event){
         for(Survivor survivor: this.survivorList){
-            int distance = this.distance(event.getLocation(), survivor.getLocation());
+            int distance = this.getDistance(event.getLocation(), survivor.getLocation());
             switch (survivor.getType()) {
                 case "citizen":
                 case "merchant":
                     if (distance <= ZOMBIE_CITIZEN_MERCHANT_DISTANCE) {
-                        Point runLocation = this.findRunLocation(survivor.getLocation(), event.getLocation());
+                        Point runLocation = this.getRunLocation(survivor.getLocation(), event.getLocation());
                         survivor.notify(new Event("run", runLocation));
                     }
+                    //this.notifySurvivor(event, survivor, ZOMBIE_CITIZEN_MERCHANT_DISTANCE);
                     break;
                 case "soldier":
-                    if (distance <= ZOMBIE_SOLDIER_DISTANCE) {
-                        survivor.notify(event);
-                    }
+                    this.notifySurvivor(event, survivor, ZOMBIE_SOLDIER_DISTANCE);
             }
         }
     }
 
-    private int distance(Point p1, Point p2){
-        return (int)Math.sqrt(this.square((p2.getX()-p1.getX())) + square(p2.getY()-p1.getY()));
+    private void notifySurvivor(Event event, Survivor survivor, int maxDistance){
+        int distance = this.getDistance(event.getLocation(), survivor.getLocation());
+        if (distance <= maxDistance) {
+            survivor.notify(event);
+        }
     }
 
-    private double square(double num1){
+    private int getDistance(Point p1, Point p2){
+        return (int)Math.sqrt(this.getSquare((p2.getX()-p1.getX())) + getSquare(p2.getY()-p1.getY()));
+    }
+
+    private double getSquare(double num1){
         return num1*num1;
     }
 
-    private Point findRunLocation(Point survivor, Point zombie){
+    private Point getRunLocation(Point survivor, Point zombie){
         float angle = this.getAngle(survivor, zombie);
         double x = survivor.getX() + (Math.cos(angle));
         double y = (survivor.getY() + Math.sin(angle));
@@ -124,7 +121,6 @@ public class Application {
 
     private float getAngle(Point survivor, Point zombie){
         float angle = (float) Math.toDegrees(Math.atan2(survivor.getY() - zombie.getY(), survivor.getX() - zombie.getX()));
-
         if(angle < 0){
             angle += 360;
         }
--- src/immortuos/utils/Soldier.java
+++ src/immortuos/utils/Soldier.java
@@ -3,6 +3,7 @@ package immortuos.utils;
 public class Soldier implements Survivor {
 
     private String type = "soldier";
+    private Point location;
 
     @Override
     public void notify(Event event) {
@@ -11,7 +12,7 @@ public class Soldier implements Survivor {
 
     @Override
     public Point getLocation() {
-        return null;
+        return location;
     }
 
     @Override
